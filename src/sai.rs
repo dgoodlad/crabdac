cad6408ff18ddbc06d088c078afa48de255ce9ca
David Goodlad <david@goodlad.net>
2022-01-11 19:36:43 +1100

    Start specifying pins & roles for SAI

--- src/sai.rs
+++ src/sai.rs
@@ -1,4 +1,4 @@
-use core::marker::PhantomData;
+use core::{marker::PhantomData, ops::Deref};
 
 use stm32f4xx_hal::{
     dma::{
@@ -10,9 +10,151 @@ use stm32f4xx_hal::{
         Stream6,
         Stream7,
         MemoryToPeripheral,
-        PeripheralToMemory, Transfer,
+        PeripheralToMemory,
     },
-    pac::{DMA2, SAI1, SAI2}};
+    gpio::{
+        gpioa,
+        gpiob,
+        gpioc,
+        gpiod,
+        gpioe,
+        gpiof,
+        gpiog,
+    },
+    pac::{
+        sai1,
+        DMA2,
+        SAI1,
+        SAI2,
+        RCC,
+    },
+    rcc::{self, Clocks}, gpio::{Alternate, PushPull, Pin}
+};
+
+// Implemented by all SAI instances
+pub trait Instance:
+    Deref<Target = sai1::RegisterBlock>
+{
+    #[doc(hidden)]
+    fn ptr() -> *const sai1::RegisterBlock;
+}
+
+macro_rules! sai {
+    ($SAI:ident) => {
+        impl Instance for $SAI {
+            fn ptr() -> *const sai1::RegisterBlock {
+                <$SAI>::ptr() as *const _
+            }
+        }
+    };
+}
+
+sai! { SAI1 }
+sai! { SAI2 }
+
+pub struct Sai<SAI> {
+    sai: SAI,
+}
+
+impl<SAI> Sai<SAI>
+where
+    SAI: Instance,
+{
+    pub fn new(sai: SAI, clocks: &Clocks) -> Sai<SAI> {
+        Sai { sai }
+    }
+
+    // pub fn split(self) -> Blocks {
+    //     unsafe {
+    //         let rcc = &(*RCC::ptr());
+
+    //         self.0
+    //     }
+    // }
+}
+
+pub struct Mclk;
+pub struct Sck;
+pub struct Fs;
+pub struct Sd;
+
+pub trait Pins<SAI, BLOCK> {}
+
+impl<SAI, BLOCK, SCK, FS, SD> Pins<SAI, BLOCK>
+    for (SCK, FS, SD)
+where
+    SCK: SaiPin<SAI, BLOCK, Sck>,
+    FS: SaiPin<SAI, BLOCK, Fs>,
+    SD: SaiPin<SAI, BLOCK, Sd>,
+{}
+
+impl<SAI, BLOCK, SCK, FS, SD, MCLK> Pins<SAI, BLOCK>
+    for (SCK, FS, SD, MCLK)
+where
+    SCK: SaiPin<SAI, BLOCK, Sck>,
+    FS: SaiPin<SAI, BLOCK, Fs>,
+    SD: SaiPin<SAI, BLOCK, Sd>,
+    MCLK: SaiPin<SAI, BLOCK, Mclk>,
+{}
+
+pub struct NoPins;
+impl Pins<SAI1, A> for NoPins {}
+impl Pins<SAI1, B> for NoPins {}
+impl Pins<SAI2, A> for NoPins {}
+impl Pins<SAI2, B> for NoPins {}
+
+pub trait SaiPin<Pin, Block, SAI> {}
+
+macro_rules! sai_pin {
+    ( $(<$SAI:ty, $Block:ty, $Pin:ty> for [$($gpio:ident::$PX:ident<$A:literal>),*]),*) => {
+        $(
+            $(
+                impl SaiPin<$Pin, $Block, $SAI> for $gpio::$PX<Alternate<PushPull, $A>> {}
+            )*
+        )*
+    };
+}
+
+sai_pin! {
+    <SAI1, A, Fs> for [gpioa::PA3<6>, gpioe::PE4<6>],
+    <SAI1, A, Sck> for [gpiob::PB10<6>, gpioe::PE5<6>],
+    <SAI1, A, Sd> for [gpiob::PB2<6>, gpioc::PC1<6>, gpiod::PD6<6>, gpioe::PE6<6>],
+    <SAI1, A, Mclk> for [gpioe::PE2<6>],
+    <SAI1, B, Fs> for [gpiob::PB9<6>, gpiof::PF9<6>],
+    <SAI1, B, Sck> for [gpiob::PB12<6>, gpiof::PF8<6>],
+    <SAI1, B, Sd> for [gpioa::PA9<6>, gpioe::PE3<6>, gpiof::PF6<6>],
+    <SAI1, B, Mclk> for [gpioc::PC0<6>, gpiof::PF7<6>],
+
+    <SAI2, A, Fs> for [gpiod::PD12<10>],
+    <SAI2, A, Sck> for [gpiod::PD13<10>],
+    <SAI2, A, Sd> for [gpiod::PD11<10>],
+    <SAI2, A, Mclk> for [gpioe::PE0<10>],
+    <SAI2, B, Fs> for [gpioe::PE13<10>, gpiog::PG9<10>],
+    <SAI2, B, Sck> for [gpioe::PE12<10>],
+    <SAI2, B, Sd> for [gpioe::PE11<10>, gpiof::PF11<10>, gpiog::PG10<10>],
+    <SAI2, B, Mclk> for [gpioa::PA1<10>, gpioe::PE14<10>]
+}
+
+// macro_rules! pin {
+//     ( $(<$Pin:ty, $SAI:ty, $BLOCK:ty> for [$($gpio:ident::$PX:ident<$A:literal>),*]),*) => {
+//         $(
+//             $(
+//                 impl<MODE> $Pin<$SAI, $BLOCK> for $gpio::$PX<MODE> {
+//                 }
+//             )
+//         )
+//     };
+// }
+
+pub struct Blocks<SAI, PinsA, PinsB>
+where
+    PinsA: Pins<SAI, A>,
+    PinsB: Pins<SAI, B>,
+{
+    sai: SAI,
+    pins_a: PinsA,
+    pins_b: PinsB,
+}
 
 pub struct A;
 pub struct B;
