f42f31386bd830ffc4e6019d52f85d837cb5da88
David Goodlad <david@goodlad.net>
2022-06-16 14:53:56 +1000

    RTIC, stm32f411

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,8 @@
 #![no_main]
 #![no_std]
 
+use stm32f4xx_hal as _; // memory layout
 use defmt_rtt as _; // global logger
-
-// TODO(5) adjust HAL import
-// use some_hal as _; // memory layout
-
 use panic_probe as _;
 
 // same panicking *behavior* as `panic-probe` but doesn't print a panic message
